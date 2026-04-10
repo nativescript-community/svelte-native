bfdc85ff1f300c3c81f330943212f73df037df27
Nathan Walker <walkerrunpdx@gmail.com>
2026-04-09 20:29:44 -0700

    feat: android icons

--- demo/app/App.svelte
+++ demo/app/App.svelte
@@ -1,16 +1,16 @@
 <page>
     <actionBar title="Svelte 5 + NativeScript" class="bg-purple-800 text-white" />
     <tabView selectedIndex={selectedTab} onselectedIndexChange={(e) => selectedTab = e.value}>
-        <tabViewItem title="Reactivity" iconSource="sys://figure.jumprope.circle.fill" class="text-sm">
+        <tabViewItem title="Reactivity" iconSource={tabIcons.reactivity} class="text-sm">
             <ReactivityPage />
         </tabViewItem>
-        <tabViewItem title="Stores" iconSource="sys://storefront.fill" class="text-sm">
+        <tabViewItem title="Stores" iconSource={tabIcons.stores} class="text-sm">
             <StoresPage />
         </tabViewItem>
-        <tabViewItem title="Controls" iconSource="sys://gamecontroller.fill"class="text-sm">
+        <tabViewItem title="Controls" iconSource={tabIcons.controls} class="text-sm">
             <ControlsPage />
         </tabViewItem>
-        <tabViewItem title="About" iconSource="sys://info.circle.fill"class="text-sm">
+        <tabViewItem title="About" iconSource={tabIcons.about} class="text-sm">
             <AboutPage />
         </tabViewItem>
     </tabView>
@@ -21,6 +21,13 @@
     import StoresPage from './pages/StoresPage.svelte'
     import ControlsPage from './pages/ControlsPage.svelte'
     import AboutPage from './pages/AboutPage.svelte'
+
+    const tabIcons = {
+        reactivity: __APPLE__ ? 'sys://figure.jumprope.circle.fill' : 'res://ic_tab_reactivity',
+        stores: __APPLE__ ? 'sys://storefront.fill' : 'res://ic_tab_stores',
+        controls: __APPLE__ ? 'sys://gamecontroller.fill' : 'res://ic_tab_controls',
+        about: __APPLE__ ? 'sys://info.circle.fill' : 'res://ic_tab_about',
+    };
 
     let selectedTab = 0;
 </script>
